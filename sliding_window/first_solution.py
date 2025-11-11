ecafa1f490e474cd8b432938a16c65556557e1a7
sudipproject <sudipsen04@gmail.com>
2025-11-11 15:26:38 +0000

    Largest Sum subarray code finish

--- sliding_window/first_solution.py
+++ sliding_window/first_solution.py
@@ -8,6 +8,16 @@ def largest_sum_subarray(list, k):
     current_sum = sum(list[:k])
     max_sum = current_sum
 
+    for i in range(k,len(list)):
+        current_sum += list[i] - list[i-k]
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+            start_index = i - k + 1
+    output_list = list[start_index: start_index+k]
+    
+
+
     return output_list, max_sum
 
 my_list = [-2, -5, 6, 4, 3, 8, -1, 0, 9]
