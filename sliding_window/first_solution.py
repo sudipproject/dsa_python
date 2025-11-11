629a07e6b708f6ba680de916e4436e116df0c59d
sudipproject <sudipsen04@gmail.com>
2025-11-11 15:14:41 +0000

    Second Commit

--- sliding_window/first_solution.py
+++ sliding_window/first_solution.py
@@ -1,1 +1,15 @@
-print('ok')
+#Largest Sum subarray
+
+def largest_sum_subarray(list, k):
+    current_sum = 0
+    max_sum = 0
+    output_list = list[:k]
+    start_index = 0
+    current_sum = sum(list[:k])
+    max_sum = current_sum
+
+    return output_list, max_sum
+
+my_list = [-2, -5, 6, 4, 3, 8, -1, 0, 9]
+k = 4
+print(largest_sum_subarray(my_list, k))
