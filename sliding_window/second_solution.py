f09c842a967d174ebef8eb5c8ae0f06e471251bc
sudipproject <sudipsen04@gmail.com>
2025-11-11 15:59:36 +0000

    First Commit of Max number of vowels in subarrays of given length

---
+++ sliding_window/second_solution.py
@@ -0,0 +1,8 @@
+# Max number of vowels in subarrays of given length
+
+my_string = ['t','h','i','s','i','s','a','l','o','n','g','x']
+k = 4
+
+current_sum = 0
+max_sum = 0
+vowels = ['a','e','i','o','u','A','E','I','O','U']
